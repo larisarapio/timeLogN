6884f875744d063579b2e106bed7eb9d6c45dc5b
larisarapio <larissa.sarapio@gmail.com>
2025-07-01 23:13:11 -0300

    create: number par

---
+++ src/NumberPar.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class NumberPar  {
+
+    public static void main(String[] args) {
+
+        int number = 20;
+
+        for (int i = 0; i <= number; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+
+
+    }
+}
