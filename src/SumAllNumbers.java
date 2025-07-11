cfba9815cef193846b46a5a0cde6ac91153de1df
larisarapio <larissa.sarapio@gmail.com>
2025-07-10 23:15:57 -0300

    create: sumAllNumbers

---
+++ src/SumAllNumbers.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SumAllNumbers {
+    public static void main(String[] args) {
+       // somar todos os números de uma lista
+        List<Integer> listaNumbers = new ArrayList<Integer>();
+        int sum = 0;
+
+        listaNumbers.add(1);
+        listaNumbers.add(5);
+        listaNumbers.add(4);
+
+        for(Integer num : listaNumbers) {
+            sum += num;
+        }
+        System.out.println(sum);
+
+    }
+}
