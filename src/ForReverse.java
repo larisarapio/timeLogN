c9367437bab3a5f3ea7fa02fe5d918244104d3fe
larisarapio <larissa.sarapio@gmail.com>
2025-07-13 21:08:00 -0300

    create: forReverse

---
+++ src/ForReverse.java
@@ -0,0 +1,13 @@
+import java.sql.Array;
+
+public class ForReverse {
+    public static void main(String[] args) {
+
+        int[] numbers = {0,1,2,3,4,5,6,7,8,9,10};
+        for(int i = numbers.length -1; i >= 0; i--){
+            System.out.println(numbers[i]);
+        }
+
+    }
+
+}
