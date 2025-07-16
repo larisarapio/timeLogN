7e0c3519665297ec1893343eb50189804109efb1
larisarapio <larissa.sarapio@gmail.com>
2025-07-15 22:30:58 -0300

    create: numbersImpars

---
+++ src/NumbersImpars.java
@@ -0,0 +1,11 @@
+public class NumbersImpars {
+    public static void main(String[] args) {
+        //imprima de 1 a 30, apenas ímpares
+        for(int i = 1; i <= 30; i++) {
+            if(i % 2 == 1) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
