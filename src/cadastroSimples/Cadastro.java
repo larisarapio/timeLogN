cd2035995c10f2efaa841e86ff5dcef249c35fdd
larisarapio <larissa.sarapio@gmail.com>
2025-07-23 23:39:05 -0300

    create: cadastro - em andamento

---
+++ src/cadastroSimples/Cadastro.java
@@ -0,0 +1,18 @@
+package cadastroSimples;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cadastro {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("John");
+        names.add("Jane");
+        names.add("Bob");
+        names.add("Mary");
+        names.add("Jack");
+
+        //addicionar funçoes de remover/buscar
+
+    }
+}
